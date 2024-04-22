649a4d0ee523b770ca101792801b50708839e3c1
Gremory08 <angel.soserr04@gmail.com>
2024-04-21 20:16:05 -0600

    modify features

--- src/reading/domain/Reading.ts
+++
@@ -1,8 +0,0 @@
-export class Reading {
-    constructor(
-        readonly id: number,
-        readonly power_usage: number,
-        readonly room_id: number,
-        readonly updated_at: string,
-    ) {}
-}
--- src/reading/domain/ReadingRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Reading } from './Reading';
-
-export interface ReadingRepository {
-    save(reading: Reading): Promise<Reading | null>;
-    findAll(): Promise<Reading[]>;
-}
